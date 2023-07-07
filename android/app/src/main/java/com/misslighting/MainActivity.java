6c1f32a01e192682cd7b34529927cfd385746767
aka.orihero <aka.orihero@gmail.com>
2023-07-08 00:11:33 +0500

    screens ready

--- android/app/src/main/java/com/misslighting/MainActivity.java
+++ android/app/src/main/java/com/misslighting/MainActivity.java
@@ -1,4 +1,4 @@
-package uz.misslighting;
+package com.nova.max.ru;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Misslighting";
+    return "NovaMax";
   }
 
   /**
